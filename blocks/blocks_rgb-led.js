34ab644bcc8ba1ba60ebcacfd1c2faf55c71e895
Gr4vity4 <prawared.bw@gmail.com>
2019-08-22 11:59:23 +0700

    add blocks

---
+++ blocks/blocks_rgb-led.js
@@ -0,0 +1,21 @@
+Blockly.Blocks['rgb_block'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("RGB LED");
+    this.appendValueInput("RGB_RED_PIN")
+        .setCheck(null)
+        .appendField("RED PIN");
+    this.appendValueInput("RGB_BLUE_PIN")
+        .setCheck(null)
+        .appendField("BLUE PIN");
+    this.appendValueInput("RGB_GREEN_PIN")
+        .setCheck(null)
+        .appendField("GREEN PIN");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ blocks/generators_rgb-led.js
@@ -0,0 +1,8 @@
+Blockly.JavaScript['rgb_block'] = function(block) {
+  var value_rgb_red_pin = Blockly.JavaScript.valueToCode(block, 'RGB_RED_PIN', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_rgb_blue_pin = Blockly.JavaScript.valueToCode(block, 'RGB_BLUE_PIN', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_rgb_green_pin = Blockly.JavaScript.valueToCode(block, 'RGB_GREEN_PIN', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
